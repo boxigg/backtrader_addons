314d9c96b140b702b5b41462fbacc73a65dcb3e9
Andrey Blinkov <andreyblinkov@gmail.com>
2018-08-19 22:35:47 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='backtrader_addons',
+      version='0.0.0',
+      description='Helpful addons (analyzers, observers, indicators etc) for backtrader',
+      url='http://github.com/storborg/funniest',
+      author='ab-trader',
+      author_email='',
+      license='MIT',
+      packages=['backtrader_addons'],
+      zip_safe=False)
