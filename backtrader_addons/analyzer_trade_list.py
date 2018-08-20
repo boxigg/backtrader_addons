fbb084a9f59c249bfe6dddc2225165934e56a79f
Andrey Blinkov <andreyblinkov@gmail.com>
2018-08-20 07:32:45 -0700

    Release 0.1.1

---
+++ backtrader_addons/analyzer_trade_list.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2018 ab-trader
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import backtrader as bt
+
+class trade_list(bt.Analyzer):
+
+    def get_analysis(self):
+
+        return self.trades
+
+
+    def __init__(self):
+
+        self.trades = []
+        self.cumprofit = 0.0
+
+
+    def notify_trade(self, trade):
+
+        if trade.isclosed:
+
+            brokervalue = self.strategy.broker.getvalue()
+
+            dir = 'short'
+            if trade.history[0].event.size > 0: dir = 'long'
+
+            pricein = trade.history[len(trade.history)-1].status.price
+            priceout = trade.history[len(trade.history)-1].event.price
+            datein = bt.num2date(trade.history[0].status.dt)
+            dateout = bt.num2date(trade.history[len(trade.history)-1].status.dt)
+            if trade.data._timeframe >= bt.TimeFrame.Days:
+                datein = datein.date()
+                dateout = dateout.date()
+
+            pcntchange = 100 * priceout / pricein - 100
+            pnl = trade.history[len(trade.history)-1].status.pnlcomm
+            pnlpcnt = 100 * pnl / brokervalue
+            barlen = trade.history[len(trade.history)-1].status.barlen
+            pbar = pnl / barlen
+            self.cumprofit += pnl
+
+            size = value = 0.0
+            for record in trade.history:
+                if abs(size) < abs(record.status.size):
+                    size = record.status.size
+                    value = record.status.value
+
+            highest_in_trade = max(trade.data.high.get(ago=0, size=barlen+1))
+            lowest_in_trade = min(trade.data.low.get(ago=0, size=barlen+1))
+            hp = 100 * (highest_in_trade - pricein) / pricein
+            lp = 100 * (lowest_in_trade - pricein) / pricein
+            if dir == 'long':
+                mfe = hp
+                mae = lp
+            if dir == 'short':
+                mfe = -lp
+                mae = -hp
+
+            self.trades.append({'ref': trade.ref, 'ticker': trade.data._name, 'dir': dir,
+                 'datein': datein, 'pricein': pricein, 'dateout': dateout, 'priceout': priceout,
+                 'chng%': round(pcntchange, 2), 'pnl': pnl, 'pnl%': round(pnlpcnt, 2),
+                 'size': size, 'value': value, 'cumpnl': self.cumprofit,
+                 'nbars': barlen, 'pnl/bar': round(pbar, 2),
+                 'mfe%': round(mfe, 2), 'mae%': round(mae, 2)})
